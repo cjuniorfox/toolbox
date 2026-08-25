21ca601f54451dd5b3ef256f4268ce4e6db47f95
Carlos Junior <cjuniorfox@gmail.com>
2026-08-25 12:22:51 -0300

    Update install.sh

--- snid/install.sh
+++ snid/install.sh
@@ -26,9 +26,11 @@ done <<< $(echo "$PUBLIC_IP" | tr ':' '\n');
 net_parts+=("4646::")
 arr=("a" "b" "c")
 
-IFS=':'
-NAT46_PREFIX="${net_parts[*]}"
 
+if [[ -z "$NAT46_PREFIX" ]]; then
+	IFS=':'
+	NAT46_PREFIX="${net_parts[*]}"
+fi
 echo "NAT46 prefix to be used: $NAT46_PREFIX"
 
 if [[ -z "$BACKEND_CIDR" ]]; then
@@ -43,13 +45,13 @@ Description=SNI TLS Proxy Daemon
 After=network-online.target
 
 [Service]
-ExecStartPre=/usr/sbin/ip -6 route replace local fd38:82fc:4248:46:4646::/96 dev lo
+ExecStartPre=/usr/sbin/ip -6 route replace local $NAT46_PREFIX::/96 dev lo
 # Add this route only if using BGP (frr)
-ExecStartPre=/usr/sbin/ip -6 route replace fd38:82fc:4248:46:4646::/96 dev lo
-ExecStart=/usr/local/sbin/snid -listen tcp:0.0.0.0:443 -mode nat46 -nat46-prefix fd38:82fc:4248:46:4646:: -backend-cidr 2804:1124:fd00::/40
+ExecStartPre=/usr/sbin/ip -6 route replace $NAT46_PREFIX::/96 dev lo
+ExecStart=/usr/local/sbin/snid -listen tcp:0.0.0.0:443 -mode nat46 -nat46-prefix $NAT46_PREFIX:: -backend-cidr $BACKEND_CIDR
 # Add this route only if using BGP (frr)
-ExecStopPost=/usr/sbin/ip -6 route del fd38:82fc:4248:46:4646::/96 dev lo
-ExecStopPost=/usr/sbin/ip -6 route del local fd38:82fc:4248:46:4646::/96 dev lo
+ExecStopPost=/usr/sbin/ip -6 route del $NAT46_PREFIX::/96 dev lo
+ExecStopPost=/usr/sbin/ip -6 route del local $NAT46_PREFIX::/96 dev lo
 Restart=always
 
 [Install]
